29a83dfbff5ad9df224c0b0415373a4ffbcd7c24
Grigary1 <grigaryvarkey16@gmail.com>
2025-10-05 17:36:55 +0530

    feat: add PatientController with basic GET and POST endpoints

--- src/main/java/com/hospitalmanagement/backend/controllers/PatientControllers.java
+++ src/main/java/com/hospitalmanagement/backend/controllers/PatientControllers.java
@@ -9,12 +9,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/patients")
 public class PatientControllers {
+    //to get the list of all patients
     @GetMapping("/patient")
     public List<Patient> getPatients(){
         System.out.println("Fetching all patients");
         return null;
     }
-
+    //to create new patient
     @PostMapping("/patient")
     public Patient createPatient(@Valid @RequestBody Patient patient){
         System.out.println(patient);
